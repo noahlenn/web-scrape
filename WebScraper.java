e87656daf7680bdca64b80da6f4712418e3fab0f
noahlenn <noahlenn@hotmail.com>
2023-10-19 18:21:44 +0200

    Test jsoup

---
+++ WebScraper.java
@@ -0,0 +1,27 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+public class WebScraper {
+    public static void main(String[] args){
+
+        try {
+            String url = "https://www.vg.no/";
+            Document document = Jsoup.connect(url).get();
+            
+            // Hent alle overskrifter fra siden
+            Elements headers = document.select("h1, h2, h3, h4, h5, h6");
+            
+            // Print ut all informasjon med headers
+            for (Element header : headers) {
+                System.out.println(header.text());
+            }
+        } catch (Exception e) {
+            System.out.println(e);    
+        }
+
+    }
+    
+}
