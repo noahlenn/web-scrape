8ff6d000247f71792b37c947f65d0ddce8593d05
noahlenn <noahlenn@hotmail.com>
2023-10-22 21:22:16 +0200

    Oppgave ferdig

--- JsoupTester.java
+++ JsoupTester.java
@@ -6,6 +6,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.nodes.DataNode;
 import org.jsoup.select.Elements;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
+
+
 public class JsoupTester {
 
     public static void main(String[] args) {
@@ -19,11 +24,14 @@ public class JsoupTester {
             // Opprett Jackson mapper for JSON parsing
             ObjectMapper objectMapper = new ObjectMapper();
 
+            // Opprett tom liste for å legge inn artiklene senere
+            List<Article> articles = new ArrayList<>();
+
             // Går gjennom elementer med tagname: "script"
             // og trekker ut JSON-data
             for (Element element : scriptElements) {
                 for (DataNode node : element.dataNodes()) {
-                    
+
                     String jsonContent = node.getWholeData();
 
                     // Sjekk om innholdet ser ut som JSON-data
@@ -31,22 +39,22 @@ public class JsoupTester {
                         try {
                             // Forsøker å parse JSON-data fra jsonContent ved hjelp av objectMapper
                             JsonNode jsonNode = objectMapper.readTree(jsonContent);
-                            
-                            
+
                             // Sjekk om JSON-data inneholder "articleId"
                             if (jsonNode.has("articleId")) {
                                 JsonNode changesNode = jsonNode.get("changes");
                                 if (changesNode != null && changesNode.has("firstPublished")) {
-                                    // Tid i ISO 8601 
+                                    // Tid i ISO 8601
                                     String timestamp = changesNode.get("firstPublished").asText();
 
                                     // Sjekk om JSON-data inneholder "teaserText"
                                     // Her kan vi finne tittelen
                                     if (jsonNode.has("teaserText")) {
                                         String title = jsonNode.get("teaserText").asText();
-                                        title = title.replace("\n", "");
-                                        System.out.println(timestamp);
-                                        System.out.println(title);
+                                        //title = title.replace("\n", "");
+
+                                        // legg artiklene i en lista
+                                        articles.add(new Article(timestamp, title));
                                     }
                                 }
                             }
@@ -56,8 +64,53 @@ public class JsoupTester {
                     }
                 }
             }
+
+
+            // Sorter artikler fra eldst til nyest
+            articles.sort(Comparator.comparing(Article::getTimestamp));
+
+            /*
+            // psuedokode nye artikler etter første henting av artikler
+            while(true){
+                try {
+                    //finn nye artikler
+                    articles.add(new Article(timestamp, title));
+            //sleep 15sek
+                }
+            }
+            */
+
+            // Print sortert liste
+            for (Article article : articles) {
+                System.out.println(article.getTimestamp());
+                System.out.println(article.getTitle());
+                System.out.println("");
+            }
+
+            
+
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+}
+
+
+class Article {
+    private String timestamp;
+    private String title;
+
+    public Article(String timestamp, String title) {
+        this.timestamp = timestamp;
+        this.title = title;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public String getTitle() {
+        return title;
     }
 }
--- WebScraper.java
+++ WebScraper.java
@@ -1,43 +1,127 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.DataNode;
 import org.jsoup.select.Elements;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
+
+
+/*
+ * Program som gjør periodisk web scrape av vg.no.
+ * Tar inn publiseringstidspunkt og tittel, som sortes fra eldst til nyest.
+ * Første scrape viser alle artikler, videre scrapes viser bare nye artikler. 
+ * Programmet kjører i uendelig loop. Avbrytes med ctrl+c
+ * 
+ * Mangler: ingress, noen doble mellomrom, tidspunktet kan printes i enklere format
+ */
 
 
 public class WebScraper {
 
     public static void main(String[] args) {
-
         try {
-            String url = "https://www.vg.no/";
-            Document document = Jsoup.connect(url).get();
-            
-            // Hent overskrifter fra siden
-            //Elements headers = document.select("h1, h2, h3, h4, h5, h6");
-            Elements headers = document.select("h2");
+            final String url = "https://www.vg.no/";
 
-            //System.out.println(headers.size());
-            
-            // Print ut all informasjon med headers
-            for (Element header : headers) {
-                System.out.println(header.text());
+            while (true) {
+                List<Article> articles = scrape(url);
+
+                // Sorter artikler fra eldst til nyest
+                articles.sort(Comparator.comparing(Article::getTimestamp));
+
+                // Printer artiklene og legger til en tom linje 
+                for (Article article : articles) {
+                    System.out.println(article.getTimestamp());
+                    System.out.println(article.getTitle());
+                    System.out.println("");
+                }
+
+                // Sov 15 sekunder
+                Thread.sleep(15 * 1000);
             }
         } catch (Exception e) {
-            System.out.println(e);    
+            e.printStackTrace();
         }
+    }
 
-        /*
-        // psuedokode nye artikler etter første henting av artikler
-        while(true){
-            try {
-            //finn nye artikler
+    private static List<Article> scrape(String url) {
+        
+        List<Article> articles = new ArrayList<>();
 
-            //sleep 15sek
+        try {
+            // Koble til URL og hent innholdet
+            Document document = Jsoup.connect(url).get();
+            
+            // Finner elementer med tagname: "script"
+            Elements scriptElements = document.getElementsByTag("script");
+
+            // Opprett Jackson mapper for JSON parsing
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // Går gjennom elementer med tagname: "script"
+            // og trekker ut JSON-data
+            for (Element element : scriptElements) {
+                for (DataNode node : element.dataNodes()) {
+                    String jsonContent = node.getWholeData();
+
+                    // Sjekk om innholdet ser ut som JSON-data
+                    if (jsonContent.startsWith("{") || jsonContent.startsWith("[")) {
+                        try {
+                            // Forsøker å parse JSON-data fra jsonContent ved hjelp av objectMapper
+                            JsonNode jsonNode = objectMapper.readTree(jsonContent);
+                            
+                            // Sjekk om JSON-data inneholder "articleId"
+                            if (jsonNode.has("articleId")) {
+                                JsonNode changesNode = jsonNode.get("changes");
+                                
+                                if (changesNode != null && changesNode.has("firstPublished")) {
+                                    // Tid i ISO 8601
+                                    String timestamp = changesNode.get("firstPublished").asText();
+                                    
+                                    // Sjekk om JSON-data inneholder "teaserText"
+                                    // Her kan vi finne tittelen
+                                    if (jsonNode.has("teaserText")) {
+                                        String title = jsonNode.get("teaserText").asText();
+                                        title = title.replace("\n", " ");
+                                        
+                                        // Legg artiklene i en lista
+                                        articles.add(new Article(timestamp, title));
+                                    }
+                                }
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
             }
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-         */
+        return articles;
+    }
+}
 
-   }
-   
+// Artikkel-klassesom brukes til å samle artiklene i liste 
+class Article {
+    private String timestamp;
+    private String title;
+
+    
+    public Article(String timestamp, String title) {
+        this.timestamp = timestamp;
+        this.title = title;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
